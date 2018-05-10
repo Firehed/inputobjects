7c6b9e06b08063f412ce9876ef3d58b54ce2f1de
Eric Stern <github@ericstern.com>
2018-05-10 14:45:39 -0700

    Allow objects to work as intended in test trait (#15)

--- tests/InputObjectTestTrait.php
+++ tests/InputObjectTestTrait.php
@@ -63,7 +63,12 @@ trait InputObjectTestTrait
     public function testEvaluate($input_value, $expected_output)
     {
         $inputObject = $this->getInputObject();
-        $this->assertSame(
+        if (is_object($expected_output)) {
+            $assert = [$this, 'assertEquals'];
+        } else {
+            $assert = [$this, 'assertSame'];
+        }
+        $assert(
             $expected_output,
             $inputObject->setValue($input_value)->evaluate(),
             'Evaluated value did not match the expected output'
