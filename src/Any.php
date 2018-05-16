8c7735b04f4c73301c6c1987f9ba9fbd54f3e6c1
Eric Stern <eric@ericstern.com>
2018-05-15 21:35:55 -0700

    Add docblock

--- src/Any.php
+++ src/Any.php
@@ -5,6 +5,10 @@ namespace Firehed\InputObjects;
 
 use Firehed\Input\Objects\InputObject;
 
+/**
+ * A validator to allow any data through. Use of this is generally discouraged
+ * for day-to-day use, but helps support some edge cases.
+ */
 class Any extends InputObject
 {
     public function validate($value): bool
