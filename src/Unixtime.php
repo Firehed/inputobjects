b2c30e34d5f993beb9d1424af10995cb7bac56ee
Eric Stern <eric@ericstern.com>
2018-06-21 09:39:01 -0700

    Add Unixtime validator

---
+++ src/Unixtime.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\InputObjects;
+
+use DateTimeImmutable;
+
+/**
+ * Takes a number input, treats it as a Unix timestamp, and evaluates into
+ * a DateTimeImmutable object. Clients SHOULD provide sub-second resolution
+ * timestamps as a string rather than actual float to avoid precision issues.
+ */
+class Unixtime extends Number
+{
+
+    public function __construct(bool $defaultToNow = false)
+    {
+        parent::__construct();
+        if ($defaultToNow) {
+            $this->setDefaultValue(new DateTimeImmutable());
+        }
+    }
+
+    public function evaluate()
+    {
+        $timestamp = $this->getValue();
+        // This logic is a little janky because floating point precision issues
+        // come up in the billion range. What this basically ends up implying
+        // is that if you want to use fractional unixtime, pass the value as
+        // a string (this only comes up in JSON and other typed input methods
+        // that can actually submit an actual number type. Any
+        // second-resolution timestamp should be fine.
+        //
+        // This does _not_ call the parent evaluation method to avoid casting
+        // to float and creating precision issues.
+        if ((int)$timestamp == $timestamp) {
+            $format = 'U';
+        } else {
+            $format = 'U.u';
+        }
+        return DateTimeImmutable::createFromFormat($format, (string) $timestamp);
+    }
+}
---
+++ tests/UnixtimeTest.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\InputObjects;
+
+use DateTimeImmutable;
+
+/**
+ * @coversDefaultClass Firehed\InputObjects\Unixtime
+ * @covers ::<protected>
+ * @covers ::<private>
+ */
+class UnixtimeTest extends \PHPUnit\Framework\TestCase
+{
+    use InputObjectTestTrait;
+
+    public function getInputObject()
+    {
+        return new Unixtime();
+    }
+
+    public function evaluations(): array
+    {
+        return [
+            // This does _not_ test float inputs as native types since it is
+            // not recommended to provide them that way in order to avoid
+            // precision issues.
+            ['1500000000.999999', new DateTimeImmutable('2017-07-14 02:40:00.999999 UTC')],
+            ['1500000000.999', new DateTimeImmutable('2017-07-14 02:40:00.999000 UTC')],
+            ['1500000000', new DateTimeImmutable('2017-07-14 02:40:00.000000 UTC')],
+            [1500000000, new DateTimeImmutable('2017-07-14 02:40:00.000000 UTC')],
+            [0, new DateTimeImmutable('1970-01-01 00:00:00.000000 UTC')],
+            [-1500000000, new DateTimeImmutable('1922-06-20 21:20:00.000000 UTC')],
+            ['-1500000000', new DateTimeImmutable('1922-06-20 21:20:00.000000 UTC')],
+            ['-1500000000.999', new DateTimeImmutable('1922-06-20 21:20:00.999000 UTC')],
+            ['-1500000000.999999', new DateTimeImmutable('1922-06-20 21:20:00.999999 UTC')],
+        ];
+    }
+
+    public function invalidEvaluations(): array
+    {
+        return [
+            [null],
+            [true],
+            [false],
+            [[]],
+            [''],
+            ['fifty'],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getDefaultValue
+     */
+    public function testConstructWithNothing()
+    {
+        $ut = new Unixtime();
+        $this->assertNull($ut->getDefaultValue());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getDefaultValue
+     */
+    public function testConstructWithFalse()
+    {
+        $ut = new Unixtime(false);
+        $this->assertNull($ut->getDefaultValue());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::getDefaultValue
+     */
+    public function testConstructWithTrue()
+    {
+        $ut = new Unixtime(true);
+        $dt = $ut->getDefaultValue();
+        $this->assertInstanceOf(DateTimeImmutable::class, $dt);
+        $diff = $dt->diff(new DateTimeImmutable());
+        // On an absurdly slow system this test could fail, so this gives it
+        // a little flexibility
+        $this->assertTrue(($diff->s + $diff->f) < 1, 'Should have been < 1sec from now');
+        $this->assertSame(0, $diff->i, 'Minutes != 0');
+        $this->assertSame(0, $diff->h, 'Hours != 0');
+        $this->assertSame(0, $diff->d, 'Days != 0');
+        $this->assertSame(0, $diff->m, 'Months != 0');
+        $this->assertSame(0, $diff->y, 'Years != 0');
+    }
+}
